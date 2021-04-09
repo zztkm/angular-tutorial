4c63beaffbc80fe982532b6c9208c6d4ea5f8008
TakumiTsuruta <ttsuruta@landscape.co.jp>
2021-04-09 15:03:40 +0900

    add: service

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  }
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+}
